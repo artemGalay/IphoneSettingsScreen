898a88279c65682b0ec67e114e79ab5fb0dee058
Artem Galay <iostema@mail.ru>
2022-08-27 12:58:24 +0300

    Register DefaultTableViewCell.

--- IphoneSettingsScreen/Sources/CustomModule/DefaultTableViewCell.swift
+++ IphoneSettingsScreen/Sources/CustomModule/DefaultTableViewCell.swift
@@ -10,28 +10,31 @@ import SnapKit
 
 class DefaultTableViewCell: UITableViewCell {
 
+    var contents: SettingCell? {
+        didSet {
+            iconImage.image = contents?.icon
+            settingLabel.text = contents?.settingLabel
+            backIconImage.backgroundColor = contents?.backgroundcolorIcon
+        }
+    }
+
     private lazy var iconImage: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
         imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10
         return imageView
     }()
 
-    private lazy var backIconImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .white
-        return imageView
-    }()
-
-    private lazy var imageContainer: UIView = {
-        let view = UIView()
+    private lazy var backIconImage: UIView = {
+        let view = UIImageView()
+        view.layer.cornerRadius = 8
         return view
     }()
 
     private lazy var settingLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont.systemFont(ofSize: 18)
         return label
     }()
 
@@ -46,26 +49,26 @@ class DefaultTableViewCell: UITableViewCell {
     }
 
     private func setupHierarchy() {
-        imageContainer.addSubview(iconImage)
-        addSubview(imageContainer)
+        addSubview(backIconImage)
+        addSubview(iconImage)
         addSubview(settingLabel)
     }
 
     private func setupLayout() {
-        iconImage.snp.makeConstraints {
-            $0.top.trailing.bottom.leading.equalTo(imageContainer)
-            $0.center.equalTo(imageContainer)
-        }
 
-        imageContainer.snp.makeConstraints {
-            $0.top.leading.bottom.equalTo(contentView).offset(10)
-            $0.centerY.equalTo(contentView)
-            $0.height.width.equalTo(60)
+        backIconImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(30)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(35)
+        }
+        
+        iconImage.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(backIconImage)
         }
 
         settingLabel.snp.makeConstraints {
-            $0.centerY.equalTo(imageContainer)
-            $0.leading.equalTo(imageContainer.snp.trailing).offset(20)
+            $0.centerY.equalTo(backIconImage)
+            $0.leading.equalTo(backIconImage.snp.trailing).offset(20)
         }
     }
 
